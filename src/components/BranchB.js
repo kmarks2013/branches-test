9139ab0416edcac7a2a7f29d2c1627fff0d93b2a
kmarks2013 <kmarks2013@gmail.com>
2020-01-29 15:45:01 -0500

    adds branch b rendering

---
+++ src/components/BranchB.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class BranchB extends Component {
+    render() {
+        return (
+            <div>
+                <h2> BRANCH B!!!!!</h2>
+            </div>
+        )
+    }
+}
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import BranchA from '../components/BranchA'
+import BranchB from '../components/BranchB'
 
 export default class MainContainer extends Component {
     render() {
         return (
             <div>
                 <BranchA />
+                <BranchB />
             </div>
         )
     }
