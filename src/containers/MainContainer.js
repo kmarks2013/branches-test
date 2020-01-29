8bf4ac5dca575d444fee983ce4d21969b80ca15e
kmarks2013 <kmarks2013@gmail.com>
2020-01-29 16:18:43 -0500

    random string

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -8,6 +8,7 @@ export default class MainContainer extends Component {
             <div>
                 <BranchA />
                 <BranchB />
+                jkwelhEGafjsd
             </div>
         )
     }
