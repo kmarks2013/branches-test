1020f6369580082935876c3245d9e77beec39bd5
kmarks2013 <kmarks2013@gmail.com>
2020-01-29 15:42:38 -0500

    adds view of branch a component

---
+++ src/components/BranchA.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class BranchA extends Component {
+    render() {
+        return (
+            <div>
+                <p> Branch A </p>
+            </div>
+        )
+    }
+}
---
+++ src/containers/MainContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import BranchA from '../components/BranchA'
+
+export default class MainContainer extends Component {
+    render() {
+        return (
+            <div>
+                <BranchA />
+            </div>
+        )
+    }
+}
