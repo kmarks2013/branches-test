a6b5ea1a45913971989a8ff063af20200f6010f4
kimberlybone <k_bone@uncg.edu>
2020-01-29 16:12:54 -0500

    Kim's changes

---
+++ src/components/BranchC.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class BranchC extends Component {
+    render() {
+        return (
+            <div>
+                <h2> BRANCH C 💎</h2>
+            </div>
+        )
+    }
+}
